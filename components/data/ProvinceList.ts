4f9276fa42235f9c08dc32924a8ae85b2bb4efe8
Abhishek Mahato <abhishek.mahato98258@gmail.com>
2024-01-26 10:19:24 +0545

    Add: province list

---
+++ components/data/ProvinceList.ts
@@ -0,0 +1,38 @@
+export const provinces = [
+    {
+      id: 1,
+      name: "Province 1",
+      value: "province1",
+    },
+    {
+      id: 2,
+      name: "Province 2",
+      value: "province2",
+    },
+    {
+      id: 3,
+      name: "Bagmati Province",
+      value: "bagmati",
+    },
+    {
+      id: 4,
+      name: "Gandaki Province",
+      value: "gandaki",
+    },
+    {
+      id: 5,
+      name: "Lumbini Province",
+      value: "lumbini",
+    },
+    {
+      id: 6,
+      name: "Karnali Province",
+      value: "karnali",
+    },
+    {
+      id: 7,
+      name: "Sudurpashchim Province",
+      value: "sudurpashchim",
+    },
+  ];
+  
