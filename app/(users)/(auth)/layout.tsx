fa8bd036c308b1072afc68f7485032c7fe0ecffc
Abhishek Mahato <abhishek.mahato98258@gmail.com>
2024-02-12 10:33:32 +0545

    feat: chage navbar active tab

--- app/(users)/(auth)/layout.tsx
+++ app/(users)/(auth)/layout.tsx
@@ -1,17 +1,45 @@
+"use client";
+
+import { useAppDispatch } from "@/redux/TypedHooks";
+import { setBarstate } from "@/redux/slices/applynewSlice";
 import { authOptions } from "@/services/authOptions";
 import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { redirect, usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession(authOptions);
-  if (!session?.user?.token) {
+  const { data: session, status } = useSession();
+  const params = useSearchParams().get("type");
+  const dispatch = useAppDispatch();
+  const location = usePathname();
+  useEffect(() => {
+    console.log(location);
+    if (location.includes("/apply/1")) {
+      dispatch(setBarstate({ active: 1, completed: [] }));
+    } else if (location.includes("/apply/2")) {
+      dispatch(setBarstate({ active: 2, completed: [1] }));
+    } else if (location.includes("/apply/3")) {
+      dispatch(setBarstate({ active: 3, completed: [1, 2] }));
+    } else if (location.includes("/apply/4")) {
+      dispatch(setBarstate({ active: 4, completed: [1, 2, 3] }));
+    } else {
+      dispatch(setBarstate({ active: 5, completed: [1, 2, 3, 4] }));
+    }
+  }, [location]);
+  if (status === "loading") return <div>Loading...</div>;
+  else if (!session?.user?.token) {
     return redirect("/login");
   } else if (session?.user?.information_id === "none") {
     return redirect("/detailform/personal");
+  } else if (session?.user?.citizenship_id === "none") {
+    return redirect("/detailform/citizenship");
+  } else if (session?.user?.license_id === "none" && params === "add") {
+    return redirect("/detailform/license");
   }
   return <>{children}</>;
 }
