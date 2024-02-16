be6fa77e27ea2c3dc69e32128a0ce4f80fc0d97d
Abhishek Mahato <abhishek.mahato98258@gmail.com>
2024-02-16 15:16:04 +0545

    feat: add admin route to create, update and delete users

---
+++ app/api/admin/users/route.ts
@@ -0,0 +1,32 @@
+import dbconnect from "@/lib/dbConnect";
+import {  User } from "@/models/userModel";
+import ShowError from "@/utils/ShowError";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req:NextRequest){
+    try {
+        await dbconnect();
+        const users = await User.find().select('-password');
+        if(!users){
+            return ShowError(400, "No users found");
+        }
+        return NextResponse.json(users, {status: 200});
+        
+    } catch (error:any) {
+        return ShowError(500, error.message)
+    }
+}
+
+export async function PUT(req:NextRequest){
+    try {
+        await dbconnect();
+        const {id, content} = await req.json();
+        const user = await User.findByIdAndUpdate(id,content, {new:true});
+        if(!user){
+            return ShowError(400, "No user found");
+        }
+        return NextResponse.json(user, {status: 200});
+    } catch (error:any) {
+        return ShowError(500, error.message)
+    }
+}
