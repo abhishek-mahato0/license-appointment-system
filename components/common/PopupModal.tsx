c5e42d344a3c9f839e3588f20414bb33315b8af3
Abhishek Mahato <abhishek.mahato98258@gmail.com>
2024-02-19 09:36:09 +0545

    Feat: add popup modal

---
+++ components/common/PopupModal.tsx
@@ -0,0 +1,68 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "../ui/button";
+
+type TPopupModal = {
+  btnclassNames?: string;
+  children: React.ReactNode;
+  btnText: string;
+  title: string;
+  triggerText: string;
+  onClick: () => void;
+  triggerClick?: () => void;
+  cancelFunction?: () => void;
+  cancelText?: string;
+  ref?: any;
+  description?: string;
+};
+
+export function PopupModal({
+  btnclassNames,
+  children,
+  btnText,
+  title,
+  triggerText,
+  onClick,
+  triggerClick,
+  cancelFunction,
+  cancelText,
+  ref,
+  description,
+}: TPopupModal) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant="outline"
+          className={btnclassNames}
+          onClick={triggerClick}
+          id="popover"
+        >
+          {triggerText}
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        {children}
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={cancelFunction}>
+            {cancelText}
+          </AlertDialogCancel>
+          <AlertDialogAction onClick={onClick}>{btnText}</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
