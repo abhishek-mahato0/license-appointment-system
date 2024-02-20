d31591bb3b4c4e7d728ec92bdab7d6a25d95edb6
Abhishek Mahato <abhishek.mahato98258@gmail.com>
2024-02-20 09:38:14 +0545

    fix: chart component

--- components/recharts/BarChart.tsx
+++ components/recharts/BarChart.tsx
@@ -1,3 +1,4 @@
+import { capitalizeFirstLetter } from "@/utils/convertDate";
 import React from "react";
 import {
   BarChart,
@@ -15,40 +16,37 @@ type TBarchart = {
   data: any;
   keys: Array<string>;
 };
+
+const fills = ["#0764D2", "#FF9525", "#FFE28A", "#FFC0CB", "#FF0000"];
 export default function CustomBarChart({ data, keys }: TBarchart) {
   return (
     <ResponsiveContainer width="100%" height="100%">
       <BarChart
-        width={500}
-        height={300}
+        width={450}
+        height={200}
         data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
+        // margin={{
+        //   top: 5,
+        //   right: 5,
+        //   left: 5,
+        //   bottom: 5,
+        // }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="name" className=" pt-10" />
         <YAxis />
         <Tooltip />
         <Legend />
-        {keys.map((key, index) => {
+        {keys?.map((key, index) => {
           return (
             <Bar
               dataKey={key}
-              fill="#8884d8"
-              activeBar={<Rectangle fill="pink" stroke="blue" />}
+              fill={fills[index]}
+              key={key}
+              // activeBar={<Rectangle fill="pink" stroke="blue" />}
             />
           );
         })}
-
-        {/* <Bar
-          dataKey="uv"
-          fill="#82ca9d"
-          activeBar={<Rectangle fill="gold" stroke="purple" />}
-        /> */}
       </BarChart>
     </ResponsiveContainer>
   );
