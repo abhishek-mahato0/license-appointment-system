c717fc4f2c9dcfae93225bd10f1741e7d07782cf
Abhishek Mahato <abhishek.mahato98258@gmail.com>
2024-02-20 09:39:30 +0545

    feat: add captalize letter function

--- utils/convertDate.ts
+++ utils/convertDate.ts
@@ -2,3 +2,7 @@ export const convertDate = (dateString: Date) => {
     const date = new Date(dateString);
     return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
   }
+
+  export const capitalizeFirstLetter=(string:string)=> {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
