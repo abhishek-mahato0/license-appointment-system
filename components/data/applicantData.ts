51781f56cb1309e4ddbfc1e1c25fa50eb1badc82
Abhishek Mahato <abhishek.mahato98258@gmail.com>
2024-02-21 10:43:22 +0545

    applicants form data

---
+++ components/data/applicantData.ts
@@ -0,0 +1,104 @@
+export const informationJsonData = [
+    {
+        id:1,
+        name:"First Name",
+        value:"first_name"
+    },
+    {
+        id:2,
+        name:"Middle Name",
+        value:"middle_name"
+    },
+    {
+        id:3,
+        name:"Last Name",
+        value:"last_name"
+    },
+    {
+        id:4,
+        name:"Email",
+        value:"email"
+    },
+    {
+        id:5,
+        name:"Phone",
+        value:"phone"
+    },
+    {
+        id:6,
+        name:"Date Of Birth",
+        value:"DOB",
+    },
+    {
+        id:7,
+        name:"Gender",
+        value:"gender",
+    },
+    {
+        id:8,
+        name:"Blood Group",
+        value:"blood_group",
+    },
+    {
+        id:9,
+        name:"Occupation",
+        value:"occupation",
+    },
+    {
+        id:10,
+        name:"Education",
+        value:"education",
+    }
+]
+
+export const licenseJsonData = [
+    {
+        id:1,
+        name:"License Number",
+        value:"license_number"
+    },
+    {
+        id:2,
+        name:"License Category",
+        value:"category"
+    },
+    {
+        id:3,
+        name:"License Issue Date",
+        value:"license_date"
+    },
+    {
+        id:4,
+        name:"License Expiry Date",
+        value:"expire_date"
+    },
+    {
+        id:5,
+        name:"License Issued By",
+        value:"office"
+    }
+]
+
+export const citizenshipJsonData = [
+    {
+        id:1,
+        name:"Citizenship Number",
+        value:"citizenship_no"
+    },
+    {
+        id:2,
+        name:"Citizenship Issue Date",
+        value:"issue_date"
+    },
+    {
+        id:3,
+        name:"Citizenship Type",
+        value:"citizenship_type"
+    },
+    {
+        id:4,
+        name:"Citizenship Issued District",
+        value:"issue_district"
+    }
+
+]
