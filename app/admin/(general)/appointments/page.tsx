0671d1b611acb2d66a5debd3def505fe6f7eb37e
Abhishek Mahato <abhishek.mahato98258@gmail.com>
2024-02-22 17:51:14 +0545

    feat: ad component to add, edit and delete office

---
+++ app/admin/(general)/appointments/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Medical from "@/components/admin/appointments/Medical";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="w-full">
+      <Medical />
+    </div>
+  );
+}
