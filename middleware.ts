bd7fd6b4efc23dd7458a1a77648698b29da22f72
Abhishek Mahato <abhishek.mahato98258@gmail.com>
2024-03-01 17:00:03 +0545

    feat: add middleware

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest } from 'next/server'
+import { getInfo } from './components/common/getUserInfo';
+import { checkLogged } from './lib/userAuth';
+  
+export async function middleware(request: NextRequest) {
+    const currentUser = request.cookies.get('token')
+    const path = request.nextUrl.pathname;
+   
+    if(!currentUser && path.startsWith('/apply')){
+        return Response.redirect(new URL('/login', request.url))
+    }
+    if(!currentUser && path.startsWith('/profile')){
+        return Response.redirect(new URL('/login', request.url))
+    }
+    if(!currentUser && path.startsWith('/admin')){
+        return Response.redirect(new URL('admin/login', request.url))
+    }
+    if(currentUser && path.startsWith('/login')){
+        return Response.redirect(new URL('/', request.url))
+    }
+}
