07e9e7dd31f99d030f438cedcae6cc2a6fd1b4a9
Abhishek Mahato <abhishek.mahato98258@gmail.com>
2024-04-01 17:21:03 +0545

    feat: add khalti api for payment

--- app/(users)/(auth)/apply/payment/page.tsx
+++ app/(users)/(auth)/apply/payment/page.tsx
@@ -1,13 +1,35 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
+import { apiinstance } from "@/services/Api";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import React from "react";
 
-export default function page() {
+export default function page({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+  const { data: session } = useSession();
+  const app_id = searchParams?.app_id;
   const { toast } = useToast();
   const router = useRouter();
-  async function khaltiPayment() {}
+  const [loading, setLoading] = React.useState(false);
+
+  let payload = {
+    return_url: `http://localhost:3000/apply/payment/verify/`,
+    website_url: "http://localhost:3000/",
+    amount: 1000,
+    purchase_order_id: app_id,
+    purchase_order_name: session?.user?.id,
+    customer_info: {
+      name: "License Application",
+      email: "example@gmail.com",
+      phone: "9800000123",
+    },
+  };
+
   function cashPayment() {
     toast({
       title: "Cash On Payment",
@@ -16,13 +38,51 @@ export default function page() {
     });
     router.push("/dashboard");
   }
+  async function makePyament() {
+    setLoading(true);
+    try {
+      const { data } = await apiinstance.post(
+        "https://a.khalti.com/api/v2/epayment/initiate/",
+        payload,
+        {
+          headers: {
+            Authorization: "key a558b8820fa84abca6fd20cf6c51a0f0",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (data) {
+        if (data?.payment_url) {
+          router.push(data?.payment_url);
+        } else {
+          toast({
+            title: "Payment Failed",
+            description: "Your payment has been failed. Please try again.",
+          });
+        }
+      }
+    } catch (error) {
+      return toast({
+        title: "Payment Failed",
+        description: "Your payment has been failed",
+      });
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
     <div className=" w-full flex flex-col items-start justify-center">
       <div className=" w-[70%] flex flex-col p-4 bg-custom-50 text-customtext-100">
         <div className=" w-full pl-10">
           <h2 className=" font-bold">Choose a Payment Method</h2>
           <div className=" w-full flex items-center justify-between mt-2 gap-[50px]">
-            <Button onClick={() => {}} className=" w-1/2 " variant="khalti">
+            <Button
+              onClick={() => {
+                makePyament();
+              }}
+              className=" w-1/2 "
+              variant="khalti"
+            >
               <img
                 src="/images/khaltis.svg"
                 alt="Khalti"
