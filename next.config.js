5e7362f000b7300d3e79c04fde56ac7fc6b0fc10
Abhishek Mahato <abhishek.mahato98258@gmail.com>
2024-04-20 11:31:44 +0545

    fix: nextconfig

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  env: {
-    WEB_URL: "http://localhost:3000/api/",
-    VERIFY_URL: "http://localhost:3000/verify/",
-    BASE_URL: "http://localhost:3000",
-    FLASK_SERVER: "http://127.0.0.1:5000",
-  },
+  // env: {
+  //   WEB_URL: "http://localhost:3000/api/",
+  //   VERIFY_URL: "http://localhost:3000/verify/",
+  //   BASE_URL: "http://localhost:3000",
+  //   FLASK_SERVER: "http://127.0.0.1:5000",
+  // },
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
